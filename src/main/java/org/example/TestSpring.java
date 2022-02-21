f658903ac30c43b3efc366129b2cded58099d77e
senseech <maxic1234@gmail.com>
2022-02-21 18:37:38 +0300

    Bean scope

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -10,10 +10,21 @@ public class TestSpring {
 
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstmusicPlayer == secondmusicPlayer;
+
+        System.out.println(comparison);
+        System.out.println(firstmusicPlayer);
+        System.out.println(secondmusicPlayer);
+        firstmusicPlayer.setVolume(10);
+        System.out.println(firstmusicPlayer.getVolume());
+        System.out.println(secondmusicPlayer.getVolume());
+
+        //musicPlayer.playMusicList();
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
